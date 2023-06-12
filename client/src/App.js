ea37b56248f6702beb55f59015adad153d1fcac1
POCHIC Olivier <o.pochic@gmail.com>
2023-06-12 17:26:21 +0200

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+
+  console.log('A feature has been added ...')
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
